d844d72921670fbbc5ec586a1d5cb76461dc6ec0
DishantSingh-Inventor <spoodermanrealx@gmail.com>
2026-06-15 20:57:41 +0530

    JS worked lowercass uppercase problem but how the hell did i wrote a h in the place of n

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     var btn = document.getElementById("hamb-btn");
     var nav = document.getElementById("nav-links-id");
     try{
-        wrongbTnName.addEventListener("click", function(){
+        wrongbtnName.addEventListener("click", function(){
             nav.classList.toggle("showMenu");
         });
     } catch(err){
 
     }
 
-    if (bth && nav){
+    if (btn && nav){
         btn.addEventListener("click", function(){
-            nav.classList.toggle("showmenu");
+            nav.classList.toggle("showMenu");
 
         });
     }
